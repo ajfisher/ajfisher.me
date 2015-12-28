3fc79d6f3f7aa28077af5a0ba49d0aede95eb64d
ajfisher <ajfisher.td@gmail.com>
2015-12-28 23:12:04 +1100

    got collections and permalinks working

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
 
 var Handlebars = require('handlebars');
+var Metalsmith = require('metalsmith');
+var collections = require('metalsmith-collections');
 var layouts = require('metalsmith-layouts');
 var markdown = require('metalsmith-markdown');
-var Metalsmith = require('metalsmith');
+var permalinks = require('metalsmith-permalinks');
 
 Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/layouts/partials/footer.hbt').toString());
 Handlebars.registerPartial('head', fs.readFileSync(__dirname + '/layouts/partials/head.hbt').toString());
@@ -11,9 +13,38 @@ Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/layouts/parti
 Handlebars.registerPartial('nav', fs.readFileSync(__dirname + '/layouts/partials/nav.hbt').toString());
 Handlebars.registerPartial('warning', fs.readFileSync(__dirname + '/layouts/partials/warning.hbt').toString());
 
+var debug = function(options) {
+    return (function(files, metalsmith, done) {
+        console.log(files);
+        console.log(metalsmith.metadata());
+        done();
+    });
+};
+
 
 Metalsmith(__dirname)
+    .use(collections({
+        pages: {
+            pattern: "pages/*.md",
+        },
+        posts: {
+            pattern: "posts/*.md",
+            sortBy: "date",
+            reverse: true,
+        },
+    }))
     .use(markdown())
+    .use(permalinks({
+        pattern: ":slug",
+        linksets: [{
+            match: {collection: "posts"},
+            pattern: ":date/:slug",
+        },
+        {
+            match: { collection: "pages"},
+            pattern: ":slug",
+        }],
+    }))
     .use(layouts({
         engine: "handlebars",
         directory: "./layouts",
