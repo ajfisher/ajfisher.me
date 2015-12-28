8725310530026693ce62c843c1e6aa4c169097fc
ajfisher <ajfisher.td@gmail.com>
2015-12-28 22:32:17 +1100

    html5 boilerplate implemented against partials

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-var Metalsmith = require('metalsmith');
-var markdown = require('metalsmith-markdown');
+var fs = require('fs');
+
+var Handlebars = require('handlebars');
 var layouts = require('metalsmith-layouts');
+var markdown = require('metalsmith-markdown');
+var Metalsmith = require('metalsmith');
+
+Handlebars.registerPartial('footer', fs.readFileSync(__dirname + '/layouts/partials/footer.hbt').toString());
+Handlebars.registerPartial('head', fs.readFileSync(__dirname + '/layouts/partials/head.hbt').toString());
+Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/layouts/partials/header.hbt').toString());
+Handlebars.registerPartial('nav', fs.readFileSync(__dirname + '/layouts/partials/nav.hbt').toString());
+Handlebars.registerPartial('warning', fs.readFileSync(__dirname + '/layouts/partials/warning.hbt').toString());
+
 
 Metalsmith(__dirname)
     .use(markdown())
