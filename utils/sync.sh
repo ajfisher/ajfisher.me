03351410dabed1c7426c0543c3ccbc2c098ff7bd
ajfisher <ajfisher.td@gmail.com>
2016-01-19 22:35:14 +1100

    fixes for mime type setting properly

--- utils/sync.sh
+++ utils/sync.sh
@@ -1,2 +1,2 @@
-s3cmd --dry-run -r --acl-public --exclude-from=utils/exclusions.txt --progress sync ./build/ s3://ajfisher.me/
-s3cmd -r --acl-public --exclude-from=utils/exclusions.txt --progress sync ./build/ s3://ajfisher.me/
+s3cmd --dry-run -r --guess-mime-type --acl-public --exclude-from=utils/exclusions.txt --progress sync ./build/ s3://ajfisher.me/
+s3cmd -r --acl-public --guess-mime-type --exclude-from=utils/exclusions.txt --progress sync ./build/ s3://ajfisher.me/
