385e65ec3930bd59a53e329b0d06c31d166827c2
ajfisher <ajfisher.td@gmail.com>
2019-11-30 18:12:34 +1100

    feat(v2): #5 building skeleton to be able to see the pages

--- site/gatsby-node.js
+++ site/gatsby-node.js
@@ -1,7 +1,46 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions, graphql }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+							slug
+              date(formatString: "YYYY/MM/DD")
+              layout
+            }
+          }
+        }
+      }
+    }
+  `)
+  if (result.errors) {
+    console.error(result.errors)
+  }
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+		console.log(node.frontmatter.slug);
+    // TODO fix this to use a date form.
+    //
+    const context = {
+      slug: node.frontmatter.slug
+    };
+
+    if (node.frontmatter.layout.startsWith('page')) {
+      createPage({
+        path: node.frontmatter.slug,
+        component: path.resolve(`src/templates/post.js`),
+        context
+      });
+    } else {
+      console.log(node.frontmatter.date + '/' + node.frontmatter.slug);
+      createPage({
+        path: node.frontmatter.date + '/' + node.frontmatter.slug,
+        component: path.resolve(`src/templates/post.js`),
+        context
+      });
+    }
+  });
+}
