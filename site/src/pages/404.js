7dc4124812da0fc98d0d79f61a0f0d8c5e75d6bb
ajfisher <ajfisher.td@gmail.com>
2020-01-27 23:03:12 +1100

    fix: remove redundant import from 404 page

--- site/src/pages/404.js
+++ site/src/pages/404.js
@@ -3,7 +3,6 @@ import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import { Header } from '../components/header';
 
 const NotFoundPage = ({location}) => {
   console.log(location);
