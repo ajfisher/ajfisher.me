7d07aa30ab6e05a903af5ca7c7d75965744b38dd
ajfisher <ajfisher.td@gmail.com>
2021-01-04 18:24:33 +1100

    feat: added dynamically created set of archive pages

---
+++ site/src/templates/post-list.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import { Link, StaticQuery, graphql } from 'gatsby';
+
+import Layout from '../components/list-layout';
+import SEO from '../components/seo';
+import { ListItems, PostListItem } from '../components/list';
+
+export default function Template({ pageContext, data}) {
+  const {limit, skip, numPages, currentPage} = pageContext;
+  const {edges} = data.allMarkdownRemark;
+  const slug = `/blog/${currentPage}`;
+
+  let featuredIndex = edges.findIndex(({node}, index) => {
+    if (node.frontmatter.featured === true) return index;
+  });
+
+  let featured;
+
+  // see if we got a featured post.
+  if (featuredIndex >= 0) {
+    featured = edges[featuredIndex].node;
+  } else {
+    // just get the first one
+    featured = edges[0].node;
+    // and set the featuredIndex to 0
+    featuredIndex = 0;
+  }
+
+  const items = edges.filter((item, index) => {
+    if (index !== featuredIndex) return item;
+  });
+
+  const seo = {
+    title: `Article archive - page ${currentPage}`,
+    description: `Article archive page ${currentPage} from ajfisher.me`
+  };
+
+  return (
+    <Layout slug={slug} featured={featured}>
+      <SEO
+        title={seo.title}
+        description={seo.description}
+        type="list"
+      />
+      <h1>Article archive - page {currentPage}</h1>
+      <ListItems>
+        {items.map(({node}) => {
+          const { slug, title, date,
+            listimage, listimage_position } = node.frontmatter;
+          const {readingTime} = node.fields;
+
+          const excerpt = node.frontmatter.excerpt || node.excerpt || null;
+
+          return (
+            <PostListItem
+              key={slug}
+              title={title}
+              date={date}
+              excerpt={excerpt}
+              slug={slug}
+              readingtime={readingTime.minutes}
+              wordcount={readingTime.words}
+              image={listimage}
+              position={listimage_position}
+            />
+          );
+        })}
+      </ListItems>
+    </Layout>
+  );
+};
+
+export const pageQuery = graphql`
+  query($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: {order: DESC, fields: frontmatter___date},
+      filter: {frontmatter: {layout: {regex: "/^post/"}}},
+      limit: $limit,
+      skip: $skip
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            slug
+            title
+            listimage
+            listimage_position
+            date(formatString: "YYYY-MM-DD")
+            excerpt
+            featured
+            featureimage
+            featureimage_position
+            small_title
+            large_title
+          }
+          excerpt(pruneLength: 220)
+          fields {
+            readingTime {
+              minutes
+              words
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
