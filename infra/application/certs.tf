2873ed1566cc8ee2f9a872fa1ef96d227696a4f6
ajfisher <ajfisher.td@gmail.com>
2021-10-23 11:25:03 +1100

    chore(tf): Fix indices changes as a result of TF upgrade to v0.13

--- infra/application/certs.tf
+++ infra/application/certs.tf
@@ -12,17 +12,25 @@ resource "aws_acm_certificate" "app_cert" {
 # sets up the dns entry to be used for verification
 resource "aws_route53_record" "app_cert_validation" {
   provider = aws
-  name     = aws_acm_certificate.app_cert.domain_validation_options[0].resource_record_name
-  type     = aws_acm_certificate.app_cert.domain_validation_options[0].resource_record_type
-  zone_id  = var.zone_id
-  records  = [aws_acm_certificate.app_cert.domain_validation_options[0].resource_record_value]
-  ttl      = 60
+  for_each = {
+    for dvo in aws_acm_certificate.app_cert.domain_validation_options : dvo.domain_name => {
+      name = dvo.resource_record_name
+      type = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  zone_id = var.zone_id
+  records = [each.value.record]
+  ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "app_cert" {
   provider                = aws.useast
   certificate_arn         = aws_acm_certificate.app_cert.arn
-  validation_record_fqdns = [aws_route53_record.app_cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.app_cert_validation: record.fqdn]
 }
 
 # add the apex domain cert now
@@ -39,16 +47,24 @@ resource "aws_acm_certificate" "apex_cert" {
 # sets up the dns entry to be used for verification
 resource "aws_route53_record" "apex_cert_validation" {
   provider = aws
-  name     = aws_acm_certificate.apex_cert.domain_validation_options[0].resource_record_name
-  type     = aws_acm_certificate.apex_cert.domain_validation_options[0].resource_record_type
-  zone_id  = var.zone_id
-  records  = [aws_acm_certificate.apex_cert.domain_validation_options[0].resource_record_value]
-  ttl      = 60
+  for_each = {
+    for dvo in aws_acm_certificate.apex_cert.domain_validation_options : dvo.domain_name => {
+      name = dvo.resource_record_name
+      type = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
+  zone_id = var.zone_id
+  records = [each.value.record]
+  ttl     = 60
 }
 
 resource "aws_acm_certificate_validation" "apex_cert" {
   provider                = aws.useast
   certificate_arn         = aws_acm_certificate.apex_cert.arn
-  validation_record_fqdns = [aws_route53_record.apex_cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.apex_cert_validation : record.fqdn]
 }
 
