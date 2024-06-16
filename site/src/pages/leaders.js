b91cf335a33de26b917ecf9ee2c5b2598ca32e77
ajfisher <ajfisher.td@gmail.com>
2024-06-16 18:51:24 +1000

    feat: new leaders page

---
+++ site/src/pages/leaders.js
@@ -0,0 +1,195 @@
+import React, { useState } from 'react';
+// import styled from 'styled-components';
+import { Link, graphql } from 'gatsby';
+
+import PageHead from '../components/page-head';
+import Layout from '../components/post-layout';
+import { getFeaturedImageSources } from '../lib/utils';
+
+export const Head = () => {
+  return (
+    <>
+      <PageHead
+        title="Leaders | ajfisher.me"
+        description="Information about leaders"
+        type="article"
+      />
+    </>
+  );
+};
+
+
+const LeadersPage = ({data}) => {
+  console.log(data.headimage.nodes[0]);
+  const {base, large, medium, small, wide, share} = data?.headimage?.nodes[0];
+
+  const featuredImageSrc = getFeaturedImageSources({
+    small, medium, large, wide, share, base
+  });
+
+  const title = 'Resources, materials and links for Leaders';
+  const slug = '/leaders';
+  const date = '2024-06-16 14:00:00+1000';
+  const excerpt = `Slides, links and other resources you'll be able to find
+    below. Don't forget to sign up if you want to get more information about
+    additional content.
+  `;
+
+  const frontmatter = {
+    title,
+    date,
+    excerpt,
+    slug,
+    large_title: true,
+  };
+
+  const submitURL = 'https://docs.google.com/forms/d/e/1FAIpQLSei22e0iBvHq5b3wrhOTetsVk_srB4a1pLzotYPKnGmi2dNYg/formResponse';
+  //const submitURL = 'https://script.google.com/macros/s/AKfycbycGh1DtxirxjS-pkiSsTkw4299LWW5HOrfoJshZMqVoIFCO5fOYEvRDtfI6QufAGWq/exec';
+
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const response = await fetch(submitURL, {
+        method: 'POST',
+        mode: 'no-cors',
+        headers:{
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          'entry.1670649209': email
+        })
+      });
+
+      console.log(response.status);
+      if (response.status === 0) {
+        // lets assumer we're okay
+        setMessage("Email submitted successfully!");
+        setEmail("");
+      } else {
+        setMessage("Submission failed. Please try again.");
+      }
+
+    } catch (error) {
+      setMessage("An error occurred: " + error.message);
+    }
+  };
+
+// const Layout = ({ children, frontmatter, featuredimage, path, readingTime, tags, relatedposts}) => {
+  return (
+    <Layout frontmatter={frontmatter} tags={['leadership', 'resources']}
+      path='/leaders' featuredimage={featuredImageSrc}
+    >
+      <PageHead title={title}/>
+      <section class="content">
+        <p>Thanks for attending my recent talk. Below you'll find some resources
+          that I think may be helpful for your leadership journey.</p>
+
+        <h3>Resources</h3>
+
+        <p>These are some resources that I've found that have been genuinely
+          helpful. If you come across something you think should be on here then
+          please reach out to me and let me know</p>
+
+        <p>
+          <ul>
+            <li>Practical explanation on <Link
+                  to="https://www.coachforsuccess.com/3-steps-to-create-relationship-maps-for-professional-success/">
+                relationship maps
+              </Link> and how to use them.
+            </li>
+            <li>How to pull together a <Link
+              to="https://www.teamagile.no/vi-deler/teamcontract">team contract</Link> /
+                social contract
+            </li>
+            <li>An alternative approach to a <Link
+              to="https://theteamcanvas.com/learn/">team canvas</Link> using a
+                web based tool
+            </li>
+            <li><Link to="https://hbr.org/">hbr.org</Link> - an outstanding
+              resource for management content. Yes, you need to get a subscription
+              but you can expense it.
+            </li>
+            <li>Why capabilities in your people are key - <Link
+              to="https://www.mckinsey.com/capabilities/people-and-organizational-performance/our-insights/to-lead-in-the-postcrisis-tomorrow-put-leadership-and-capabilities-in-place-today"
+              >McKinsey</Link>
+            </li>
+            <li>How to <Link
+              to="https://www.linkedin.com/pulse/nurturing-relationships-heart-networking-cath-andrew-duacc"
+              >nurture relationships</Link> - by Cath Andrew - a great HR leader.
+            </li>
+          </ul>
+        </p>
+
+        <h3>Subscribe to find out more</h3>
+        <p>I'm still working out the details of what type of content makes most
+          sense, however if you'd like to get notified when I release some future
+          content relating to leadership then please pop your email address in
+          below and I'll let you know (definitely no spam).</p>
+
+        <section>
+          <form onSubmit={handleSubmit}>
+            <p>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </label>
+              <button type="submit">Submit</button>
+            </p>
+          </form>
+
+          { message && <p>{message}</p>}
+        </section>
+
+        <h3>Where to next?</h3>
+
+        <p>The <Link to="/">home page</Link> will give you featured
+          articles and recent posts.</p>
+        <p>You can also check out <Link to="/blog">the full
+          list of articles</Link> that have ever been published.</p>
+
+      </section>
+    </Layout>
+  );
+};
+
+export default LeadersPage;
+
+export const pageQuery = graphql`
+  query {
+    headimage: allImageSharp(
+      filter: {original: {
+        src: {regex: "/enterprise_ai_future-/"}
+      }}
+    ) {
+      nodes {
+        id
+        base: gatsbyImageData(width: 400, quality: 100
+          transformOptions: {duotone: {highlight:"#FF5E9A", shadow:"#000000", opacity: 80}}
+        )
+        small: gatsbyImageData(width: 400, quality: 100
+          transformOptions: {duotone: {highlight:"#FF5E9A", shadow:"#000000", opacity: 80}}
+        )
+        medium: gatsbyImageData(width: 750, quality: 90
+          transformOptions: {duotone: {highlight:"#FF5E9A", shadow:"#000000", opacity: 80}}
+        )
+        large: gatsbyImageData(width: 1050, quality: 100
+          transformOptions: {duotone: {highlight:"#FF5E9A", shadow:"#000000", opacity: 80}}
+        )
+        wide: gatsbyImageData(width: 1600, quality: 100
+          transformOptions: {duotone: {highlight:"#FF5E9A", shadow:"#000000", opacity: 80}}
+        )
+        share: gatsbyImageData(width: 1200, quality: 90)
+      }
+    }
+  }
+`;
