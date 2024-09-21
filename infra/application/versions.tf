fef4b3c30e8baf1cb1506023ed8c302ca75f44be
ajfisher <ajfisher.td@gmail.com>
2024-09-21 16:48:29 +1000

    chore: update configuration for AWS provider #220

--- infra/application/versions.tf
+++ infra/application/versions.tf
@@ -7,7 +7,7 @@ terraform {
     }
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.63.0"
+      version = "~> 5.0"
       configuration_aliases = [ aws.useast ]
     }
   }
--- infra/application/web.tf
+++ infra/application/web.tf
@@ -1,66 +1,16 @@
-# policy for the bucket
-#data "template_file" "bucket_policy" {
-#  template = <<EOF
-#{
-#  "Version": "2012-10-17",
-#  "Statement": [{
-#    "Sid": "OnlyCloudfrontReadAccess",
-#    "Effect": "Allow",
-#    "Principal": {
-#      "AWS": "${aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn}"
-#    },
-#    "Action": "s3:GetObject",
-#    "Resource": "arn:aws:s3:::aj-web-ajfisher-me-${var.env}/*"
-#  }]
-#}
-#EOF
-#
-#}
-
-# data "template_file" "public_bucket_policy" {
-#   template = <<EOF
-# {
-#   "Version": "2012-10-17",
-#   "Statement": [{
-#     "Sid": "PublicRead",
-#     "Effect": "Allow",
-#     "Principal": "*",
-#     "Action": "s3:GetObject",
-#     "Resource": "arn:aws:s3:::aj-web-ajfisher-me-${var.env}/*"
-#   }]
-# }
-# EOF
-# 
-# }
-
 # Location for the frontend code to reside
 resource "aws_s3_bucket" "website_code" {
   bucket = "aj-web-ajfisher-me-${var.env}"
-  acl    = "private"
-
-  #  acl     = "public-read"
-
-  logging {
-    target_bucket = aws_s3_bucket.website_logs.id
-    target_prefix = "aj-web-logs-${var.env}/"
-  }
-
-  website {
-    index_document = "index.html"
-    error_document = "404.html"
-  }
-
-  cors_rule {
-    allowed_origins = ["https://www.ajfisher.me"]
-    allowed_headers = ["Authorization", "Content-Length"]
-    allowed_methods = ["GET", "HEAD"]
-  }
 
   tags = {
     src       = "terraform"
     component = "code"
   }
-  #  policy = data.template_file.bucket_policy.rendered
+}
+
+resource "aws_s3_bucket_policy" "website_code" {
+  bucket = aws_s3_bucket.website_code.id
+
   policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -75,13 +25,57 @@ resource "aws_s3_bucket" "website_code" {
   }]
 }
 POLICY
+}
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+resource "aws_s3_bucket_logging" "website_code" {
+  bucket = aws_s3_bucket.website_code.id
+
+  target_bucket = aws_s3_bucket.website_logs.id
+  target_prefix = "aj-web-logs-${var.env}/"
+}
+
+resource "aws_s3_bucket_website_configuration" "website_code" {
+  bucket = aws_s3_bucket.website_code.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
   }
 }
 
+resource "aws_s3_bucket_cors_configuration" "website_code" {
+  bucket = aws_s3_bucket.website_code.id
+
+  cors_rule {
+    allowed_origins = ["https://ajfisher.me"]
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "HEAD"]
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "website_code" {
+  bucket = aws_s3_bucket.website_code.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "website_code" {
+  depends_on = [aws_s3_bucket_ownership_controls.website_code]
+
+  bucket = aws_s3_bucket.website_code.id
+  acl = "private"
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "website_code" {
+  bucket = aws_s3_bucket.website_code.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
