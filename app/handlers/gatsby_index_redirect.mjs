8ed334b9f428fe023481a0af611c9b0eb24aef5c
ajfisher <ajfisher.td@gmail.com>
2024-09-22 17:32:31 +1000

    fix: add handlers for the authourl addition. Closes #225 (#226)

--- app/handlers/gatsby_index_redirect.mjs
+++ app/handlers/gatsby_index_redirect.mjs
@@ -8,11 +8,24 @@ export const handler = async (event) => {
   const request = event.Records[0].cf.request;
   const { uri } = request;
 
-  if (uri.endsWith('/')) {
+  if (uri.endsWith('%7Bauthourl%7D')) {
+    // this is dealing with a bunch of the 404 errors we see where there's a
+    // {authourl} appended to the end of the request - just remove this
+    request.uri = uri.replace('%7Bauthourl%7D', '');
+  }
+
+  if (uri.endsWith('{authourl}')) {
+    // this is dealing with a bunch of the 404 errors we see where there's a
+    // {authourl} appended to the end of the request - just remove this
+    request.uri = uri.replace('{authourl}', '');
+  }
+
+  if (request.uri.endsWith('/')) {
     request.uri += 'index.html';
-  } else if (!uri.includes('.')) {
+  } else if (!request.uri.includes('.')) {
     request.uri += '/index.html';
   }
 
   return request;
 };
+
