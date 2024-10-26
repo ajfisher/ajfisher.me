041cb91e7c1d37d0f7aa8dd4f5450e3129ab9088
ajfisher <ajfisher.td@gmail.com>
2024-10-26 17:28:15 +1100

    refactor: remove a lot of unrequired debug comments

--- site/plugins/gatsby-remark-transformer-pullquotes/index.js
+++ site/plugins/gatsby-remark-transformer-pullquotes/index.js
@@ -1,37 +1,29 @@
 const visit = require('unist-util-visit');
 const toString = require('mdast-util-to-string');
 
-module.exports = ({markdownAST}, pluginOptions) => {
+module.exports = ({markdownAST}, _pluginOptions) => {
 
-  visit(markdownAST, 'paragraph', (node, index) => {
+  visit(markdownAST, 'paragraph', (node, _index) => {
     let bfound = false;
     let bindex = null;
     node.children.forEach((child, index) => {
-      if (child.type == 'html' && child.value == '<b>') {
+      if (child.type === 'html' && child.value === '<b>') {
         bfound = true;
         bindex = index;
       }
     });
 
     if (bfound) {
-      // console.log('----- THIS IS A P with a B in it ------');
       let pullquote = node.children[bindex+1].value;
       pullquote = pullquote[0].toUpperCase() + pullquote.substring(1);
-      // if (pullquote.indexOf('"') >= 0) {
       pullquote = pullquote.replace(/"/g, '&quot;');
-      // }
+
       const text = toString(node);
-      // console.log(`>>>>> Upper cased: ${pullquote}`);
-      // console.log(`>>>>> Node text: ${text}`);
       const html = `<p class="has-pullquote" data-pullquote="${pullquote}">${text}</p>`;
-      // console.log(html);
-      // console.log('------ Node before changes -------');
-      // console.log(node);
+
       node.type = 'html';
       node.children = undefined;
       node.value = html;
-      // console.log('------ Node after changes --------');
-      // console.log(node);
     }
   });
 
