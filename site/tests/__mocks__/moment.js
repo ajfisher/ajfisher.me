5ac33b724fd29925fac0273610cf0d25d10c748a
ajfisher <ajfisher.td@gmail.com>
2025-03-13 11:18:27 +1100

    test: add tests for utils module #292

--- site/tests/__mocks__/moment.js
+++ site/tests/__mocks__/moment.js
@@ -1,8 +1,16 @@
-const mockFormat = jest.fn(() => "Mon, Jan 1st 2024");
-
-const moment = jest.fn(() => ({
-  format: mockFormat,
-}));
+// __mocks__/moment.js
+const moment = jest.fn(() => {
+  return {
+    format: (formatStr) => {
+      // If the format is "YYYY/MM/DD", return the real formatted date.
+      if (formatStr === 'YYYY/MM/DD') {
+        return "2021/08/05";
+      }
+      // Otherwise, return a default mock string.
+      return "Mon, Jan 1st 2024";
+    },
+  };
+});
 
 export default moment;
 export { moment };
