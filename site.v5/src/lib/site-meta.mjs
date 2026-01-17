f5f1edad787ce204a3a514702096701971fe2a84
ajfisher <ajfisher.td@gmail.com>
2026-01-17 17:05:29 +1100

    feat: add shared seo meta rendering

---
+++ site.v5/src/lib/site-meta.mjs
@@ -0,0 +1,6 @@
+export const SITE_META = {
+  defaultTitle: 'ajfisher',
+  description: 'The blog and portfolio of AJFisher - technologist',
+  author: '@ajfisher',
+  siteUrl: 'https://ajfisher.me',
+};
