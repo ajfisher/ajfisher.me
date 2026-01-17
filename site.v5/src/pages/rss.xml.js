f4e787916d0c6b29085efda226b09d30b947f858
ajfisher <ajfisher.td@gmail.com>
2026-01-17 22:30:37 +1100

    fix: escape rss titles and descriptions
    Refs #398

--- site.v5/src/pages/rss.xml.js
+++ site.v5/src/pages/rss.xml.js
@@ -3,6 +3,15 @@ import { getCollection } from 'astro:content';
 
 import { SITE_META } from '../lib/site-meta.mjs';
 
+const UNESCAPED_ENTITY = /&(?!amp;|lt;|gt;|quot;|apos;|#\d+;|#x[\da-fA-F]+;)/g;
+
+const escapeXmlText = (value) => value.replace(UNESCAPED_ENTITY, '&amp;');
+
+const sanitizeFeedXml = (xml) =>
+  xml.replace(/<(title|description)>([\s\S]*?)<\/\1>/g, (_match, tag, text) => (
+    `<${tag}>${escapeXmlText(text)}</${tag}>`
+  ));
+
 export async function GET(context) {
   const site = context.site ?? SITE_META.siteUrl;
   const posts = await getCollection('posts');
@@ -16,10 +25,19 @@ export async function GET(context) {
       link: new URL(`/${post.data.uri}`, site).toString(),
     }));
 
-  return rss({
+  const response = await rss({
     title: SITE_META.defaultTitle,
     description: SITE_META.description,
     site,
     items,
+  });
+
+  const xml = await response.text();
+  const sanitizedXml = sanitizeFeedXml(xml);
+
+  return new Response(sanitizedXml, {
+    status: response.status,
+    statusText: response.statusText,
+    headers: response.headers,
   });
 }
